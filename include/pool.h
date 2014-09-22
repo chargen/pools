d4a165b611181750dc6da48d6154bbd8c84cbf2b
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2014-09-21 19:52:38 -0700

    Initial Pool added

---
+++ include/pool.h
@@ -0,0 +1,213 @@
+#ifndef pool_h
+#define pool_h
+
+/*
+Copyright (c) 2014, Jeff Koftinoff <jeffk@jdkoftinoff.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#if defined( POOL_ABORT_ON_ERROR ) && !defined( POOL_ABORT )
+#define POOL_ABORT( msg )                                                                                                      \
+    do                                                                                                                         \
+    { fprintf(stderr,"%s", msg, abort();                                                                                       \
+    } while ( 0 )
+#else
+#define POOL_ABORT( msg )                                                                                                      \
+    do                                                                                                                         \
+    {                                                                                                                          \
+    } while ( 0 )
+#endif
+
+struct Pool
+{
+    /**
+     * @brief num_elements The number of elements in this pool
+     */
+    int num_elements;
+
+    /**
+     * @brief element_size The size in bytes of each element
+     */
+    int element_size;
+
+    /**
+     * @brief next_available_hint The best guess of the next available element
+     */
+    int next_available_hint;
+
+    /**
+     * @brief element_storage_size The total size in bytes of the element_storage buffer
+     */
+    int element_storage_size;
+
+    /**
+     * @brief total_allocated_items The total number of items that are currently allocated
+     */
+    int total_allocated_items;
+
+    /**
+     * @brief allocated_flags The storage for the bit map of allocated/deallocated flags. One bit per element
+     */
+    unsigned char *allocated_flags;
+
+    /**
+     * @brief element_storage The storage for all of the element
+     */
+    unsigned char *element_storage;
+
+    /**
+     * @brief diag_num_allocations Diagnostics counter for the number of allocations
+     */
+    int diag_num_allocations;
+
+    /**
+     * @brief diag_num_frees Diagnostics counter for the number of frees
+     */
+    int diag_num_frees;
+
+    /**
+     * @brief diag_num_spills Diagnostics counter for the number of spills : allocations that had to be pushed to a larger pool
+     */
+    int diag_num_spills;
+
+    /**
+     * @brief diag_multiple_allocation_errors Diagnostics counter for the number of times an element was allocated more than
+     * once at a time
+     */
+    int diag_multiple_allocation_errors;
+
+    /**
+     * @brief diag_multiple_deallocation_errors Diagnostics counter for the number of times an element was deallocated more than
+     * once at a time
+     */
+    int diag_multiple_deallocation_errors;
+
+    /**
+     * @brief low_level_allocation_function the pointer to the system's low level allocation function
+     */
+    void *( *low_level_allocation_function )( int );
+
+    /**
+     * @brief low_level_free_function The pointer to the system's low level free dunction
+     */
+    void ( *low_level_free_function )( void * );
+};
+
+/**
+ * @brief Pool_init                 Initialize a Pool
+ * @param self                      Pointer to Pool struct to initialize
+ * @param num_elements              The number of elements to allocate. May be 0 to disable Pool.
+ * @param element_size              The size of each element in bytes.May be 0 to disable Pool.
+ * @return                          -1 on error, 0 on success
+ */
+int Pool_init( struct Pool *self,
+               int num_elements,
+               int element_size,
+               void *( *low_level_allocation_function )( int ),
+               void ( *low_level_free_function )( void * ) );
+
+/**
+ * @brief Pool_terminat             Terminate a Pool and dealloate low level buffers
+ * @param self                      Pointer to the Pool to terminate
+ */
+void Pool_terminate( struct Pool *self );
+
+/**
+ * @brief Pool_allocate_element     Allocate one element from the pool
+ * @param self                      The pool to allocate from
+ * @return                          0 on failure or pointer to allocated element
+ */
+void *Pool_allocate_element( struct Pool *self );
+
+/**
+ * @brief Pool_deallocate_element   Deallocate one element from the pool
+ * @param self                      The pool to deallocate from
+ * @param p                         The pointer to deallocate
+ * @return                          -1 if the item is not allocated from this pool, or the item index if positive
+ */
+int Pool_deallocate_element( struct Pool *self, void *p );
+
+/**
+ * @brief Pool_is_element_available Check to see if a specific element index is available
+ * @param self                      The Pool to use
+ * @param element_num               The element index to check
+ * @return                          1 if the element is available, 0 otherwise.
+ */
+int Pool_is_element_available( struct Pool *self, int element_num );
+
+/**
+ * @brief Pool_mark_element_allocated   Mark the specified element as allocated
+ * @param self                          The Pool to use
+ * @param element_num                   The element index to mark as allocated
+ */
+void Pool_mark_element_allocated( struct Pool *self, int element_num );
+
+/**
+ * @brief Pool_mark_element_available   Mark the specified element as available
+ * @param self                          The Pool to use
+ * @param element_num                   The element index to mark as available
+ */
+void Pool_mark_element_available( struct Pool *self, int element_num );
+
+/**
+ * @brief Pool_get_address_for_element  Calculate the memory address of a specific element
+ * @param self                          The Pool to use
+ * @param element_num                   The element index to use
+ * @return                              Pointer to the element, or 0 if the element_num is out of range
+ */
+void *Pool_get_address_for_element( struct Pool *self, int element_num );
+
+/**
+ * @brief Pool_is_address_in_pool       Calculate if the specified address points to an element in this Pool
+ * @param self                          The Pool to use
+ * @param p                             The pointer to check
+ * @return                              1 if the address points to the beginning of an element inside this Pool, 0 otherwise
+ */
+int Pool_is_address_in_pool( struct Pool *self, void const *p );
+
+/**
+ * @brief Pool_get_element_for_address  Calculate the element number given a pointer
+ * @param self                          The Pool to use
+ * @param p                             The pointer to check
+ * @return                              The element number, or -1 if the pointer is not pointing to the beginning of an element
+ * in this Pool
+ */
+int Pool_get_element_for_address( struct Pool *self, void const *p );
+int Pool_find_next_available_element( struct Pool *self );
+
+#ifdef stdout
+/**
+ * @brief Pool_diagnostics          Print pool diagnostics counters
+ * @param self                      Pointer to Pool struct to diagnose
+ * @param f                         Pointer to FILE to print to
+ * @param prefix                    Pointer to cstring which will be put in front of each line outputted
+ */
+void Pool_diagnostics( struct Pool *self, FILE *f, const char *prefix );
+#endif
+
+#endif
---
+++ src/pool.c
@@ -0,0 +1,253 @@
+
+/*
+Copyright (c) 2014, Jeff Koftinoff <jeffk@jdkoftinoff.com>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "pool.h"
+
+int Pool_init( struct Pool *self,
+               int num_elements,
+               int element_size,
+               void *( *low_level_allocation_function )( int ),
+               void ( *low_level_free_function )( void * ) )
+{
+    int r = -1;
+    /* one bit per element */
+    int size_of_allocated_flags_in_bytes = num_elements / 8 + 1;
+    memset( self, 0, sizeof( *self ) );
+    self->low_level_allocation_function = low_level_allocation_function;
+    self->low_level_free_function = low_level_free_function;
+    self->element_size = element_size;
+    self->num_elements = num_elements;
+    self->next_available_hint = 0;
+    self->diag_num_allocations = 0;
+    self->diag_num_frees = 0;
+    self->diag_num_spills = 0;
+    self->total_allocated_items = 0;
+    self->diag_multiple_allocation_errors = 0;
+    self->diag_multiple_deallocation_errors = 0;
+    self->element_storage_size = num_elements * element_size;
+
+    if ( self->element_storage_size > 0 )
+    {
+        self->allocated_flags = (unsigned char *)self->low_level_allocation_function( size_of_allocated_flags_in_bytes );
+        if ( self->allocated_flags )
+        {
+            memset( self->allocated_flags, 0, size_of_allocated_flags_in_bytes );
+            self->element_storage = (unsigned char *)self->low_level_allocation_function( self->element_storage_size );
+            if ( self->element_size )
+            {
+                r = 0;
+            }
+            else
+            {
+                self->low_level_free_function( self->allocated_flags );
+            }
+        }
+    }
+    return r;
+}
+
+void Pool_terminate( struct Pool *self )
+{
+    if ( self->element_storage )
+    {
+        self->low_level_free_function( self->element_storage );
+    }
+    if ( self->allocated_flags )
+    {
+        self->low_level_free_function( self->allocated_flags );
+    }
+}
+
+void *Pool_allocate_element( struct Pool *self )
+{
+    void *r = 0;
+    int item = -1;
+    item = Pool_find_next_available_element( self );
+
+    if ( item != -1 )
+    {
+        Pool_mark_element_allocated( self, item );
+        r = Pool_get_address_for_element( self, item );
+        ++self->diag_num_allocations;
+    }
+    else
+    {
+        ++self->diag_num_spills;
+    }
+    return r;
+}
+
+int Pool_deallocate_element( struct Pool *self, void *p )
+{
+    int item = Pool_get_element_for_address( self, p );
+    if ( item >= 0 )
+    {
+        Pool_mark_element_available( self, item );
+        ++self->diag_num_frees;
+    }
+    return item;
+}
+
+int Pool_is_element_available( struct Pool *self, int element_num )
+{
+    int r = 1;
+    int element_div_8 = element_num / 8;
+    int element_mod_8 = element_num & 7;
+    unsigned char bit = ( 1 << element_mod_8 );
+    unsigned char mask_bit = ~bit;
+
+    if ( ( self->allocated_flags[element_div_8] & mask_bit ) != 0 )
+    {
+        r = 0;
+    }
+    return r;
+}
+
+void Pool_mark_element_allocated( struct Pool *self, int element_num )
+{
+    int element_div_8 = element_num / 8;
+    int element_mod_8 = element_num & 7;
+    unsigned char bit = ( 1 << element_mod_8 );
+    unsigned char mask_bit = ~bit;
+
+    if ( ( self->allocated_flags[element_div_8] & mask_bit ) != 0 )
+    {
+        self->diag_multiple_allocation_errors++;
+        POOL_ABORT( "Multiple allocation" );
+    }
+    else
+    {
+        self->allocated_flags[element_div_8] |= element_mod_8;
+        ++self->total_allocated_items;
+        self->next_available_hint = ( self->next_available_hint + 1 ) % self->num_elements;
+    }
+}
+
+void Pool_mark_element_available( struct Pool *self, int element_num )
+{
+    int element_div_8 = element_num / 8;
+    int element_mod_8 = element_num & 7;
+    unsigned char bit = ( 1 << element_mod_8 );
+    unsigned char mask_bit = ~bit;
+
+    if ( ( self->allocated_flags[element_div_8] & mask_bit ) == 0 )
+    {
+        self->diag_multiple_deallocation_errors++;
+        POOL_ABORT( "Multiple deallocation" );
+    }
+    else
+    {
+        self->allocated_flags[element_div_8] &= mask_bit;
+        --self->total_allocated_items;
+        self->next_available_hint = element_num;
+    }
+}
+
+void *Pool_get_address_for_element( struct Pool *self, int element_num )
+{
+    unsigned char *base = (unsigned char *)self->element_storage;
+    void *r = 0;
+    if ( element_num >= self->num_elements )
+    {
+        POOL_ABORT( "get_address_for_element out of range" );
+    }
+    r = base + ( element_num * self->element_size );
+    return r;
+}
+
+int Pool_is_address_in_pool( struct Pool *self, void const *p )
+{
+    unsigned char *base = (unsigned char *)self->element_storage;
+    unsigned char *top = base + self->element_storage_size;
+    unsigned char const *pp = (unsigned char const *)p;
+    int r = 0;
+
+    if ( base <= pp && pp < top )
+    {
+        int offset = pp - base;
+        if ( ( offset % self->element_size ) == 0 )
+        {
+            r = 1;
+        }
+    }
+    return r;
+}
+
+int Pool_get_element_for_address( struct Pool *self, void const *p )
+{
+    unsigned char *base = (unsigned char *)self->element_storage;
+    unsigned char *top = base + self->element_storage_size;
+    unsigned char const *pp = (unsigned char const *)p;
+    int r = -1;
+    if ( base <= pp && pp < top )
+    {
+        int offset = pp - base;
+        if ( ( offset % self->element_size ) == 0 )
+        {
+            r = offset / self->element_size;
+        }
+    }
+    return r;
+}
+
+int Pool_find_next_available_element( struct Pool *self )
+{
+    int r = -1;
+    if ( self->element_storage_size > 0 )
+    {
+        if ( self->total_allocated_items < self->num_elements )
+        {
+            int pos = self->next_available_hint;
+            for ( int i = 0; i < self->num_elements; ++i )
+            {
+                int item = ( pos + i ) % self->num_elements;
+                if ( Pool_is_element_available( self, item ) )
+                {
+                    r = pos;
+                    self->next_available_hint = item;
+                    break;
+                }
+            }
+        }
+    }
+    return r;
+}
+
+#ifdef stdout
+void Pool_diagnostics( struct Pool *self, FILE *f, const char *prefix )
+{
+    fprintf( f, "%selement_size                     : %d\n", prefix, self->element_size );
+    fprintf( f, "%snum_elements                     : %d\n", prefix, self->num_elements );
+    fprintf( f, "%stotal_allocated_items            : %d\n", prefix, self->total_allocated_items );
+    fprintf( f, "%sdiag_multiple_allocation_errors  : %d\n", prefix, self->diag_multiple_allocation_errors );
+    fprintf( f, "%sdiag_multiple_deallocation_errors: %d\n", prefix, self->diag_multiple_deallocation_errors );
+    fprintf( f, "%sdiag_num_allocations             : %d\n", prefix, self->diag_num_allocations );
+    fprintf( f, "%sdiag_num_frees                   : %d\n", prefix, self->diag_num_frees );
+    fprintf( f, "%sdiag_num_spills                  : %d\n\n", prefix, self->diag_num_spills );
+}
+
+#endif
